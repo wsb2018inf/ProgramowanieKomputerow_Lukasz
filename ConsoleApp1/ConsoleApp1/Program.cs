a8fcf05040871bbafc46d57215811eaf74a435fd
lukaszniewin <46331784+lukaszniewin@users.noreply.github.com>
2019-01-03 02:34:43 +0100

    zadanie 18

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            Console.WriteLine("Podaj dlugosc ciagu");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] ciag = new int[n];
+
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("podaj wartosc elementu");
+                ciag[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine("Parzyste to :");
+            foreach (var item in ciag)
+            {
+                if (item % 2 == 0)
+                    Console.WriteLine("{0}", item);
+            }
+        }
+    }
+}
